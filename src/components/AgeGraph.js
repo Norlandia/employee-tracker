afcf32930351f10f163bd792770c05fe3d7d9e50
Nora <norapuskas@yahoo.com>
2019-01-20 18:33:59 +0100

    refactor(styles.css): change some stylings

--- src/components/AgeGraph.js
+++ src/components/AgeGraph.js
@@ -3,24 +3,24 @@ import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
 
 const AgeGraph = ({ ages, show }) => {
   const isVisible = () => {
-    return show
-      ? 'graph-container graph-visible' : 'graph-container graph-hide';
+    return show ? 'graph-container graph-visible' : 'graph-container graph-hide';
   };
 
   return (
-    <div className={isVisible()}>
-      <BarChart
-        width={600}
-        height={300}
-        data={ages}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <XAxis dataKey="age" />
-        <YAxis ticks={[...ages.map((e) => e.count)]} />
-        <Tooltip cursor={{ fill: '#B5DEF3' }} />
-        <Bar dataKey="count" fill="#0984C3" />
-      </BarChart>
-    </div>
+   
+      <div className={isVisible()}>
+        <BarChart
+          width={600}
+          height={300}
+          data={ages}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="age" />
+          <YAxis ticks={[...ages.map((e) => e.count)]} />
+          <Tooltip cursor={{ fill: '#B5DEF3' }} />
+          <Bar dataKey="count" fill="#0984C3" />
+        </BarChart>
+      </div>
   );
 };
 
