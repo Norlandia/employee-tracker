e1723ca6ab813bf75f9688e08388409e8fd3576e
Nora <norapuskas@yahoo.com>
2019-01-20 22:17:05 +0100

    fix(EmployeeListApp.js): fix double deletion, change splice to filter

--- src/containers/EmployeeListApp.js
+++ src/containers/EmployeeListApp.js
@@ -17,13 +17,15 @@ class EmployeeListApp extends Component {
     const deletedEmployee = {
       action: 'DELETE',
       time: new Date().getTime(),
-      ...this.state.employees.splice(id, 1)[0],
+      ...this.state.employees.filter((employee, i) => i === id),
     };
+    console.log('n', deletedEmployee);
 
     this.setState({
       employees: this.state.employees.filter((employee, i) => i !== id),
       dump: [deletedEmployee, ...this.state.dump],
     });
+    console.log('state2', this.state.employees);
   };
 
   showModal = () => {
@@ -94,18 +96,24 @@ class EmployeeListApp extends Component {
   handleGraph = () => {
     this.setState({
       showGraph: !this.state.showGraph,
-    })
-  }
+    });
+  };
 
   render() {
+    console.log('state1', this.state.employees);
     return (
       <div className="container">
         <h1 className="page-title">Employee Tracker</h1>
         <button className="add-button" onClick={() => this.showModal()}>
           Add
         </button>
-        <AgeGraph ages={this.getAgeDistribution()} show={this.state.showGraph}/>
-        <button className="graph-button" onClick={() => this.handleGraph()}>Graph</button>
+        <AgeGraph
+          ages={this.getAgeDistribution()}
+          show={this.state.showGraph}
+        />
+        <button className="graph-button" onClick={() => this.handleGraph()}>
+          Graph
+        </button>
         <AddNewEmployee
           onSubmit={this.handleSubmit}
           show={this.state.showModal}
